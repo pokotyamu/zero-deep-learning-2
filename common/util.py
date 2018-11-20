9a4a1354aee0e1cdde64725bf39aedf0b0a0175c
pokotyamu <pokotyamu@gmail.com>
2018-11-20 18:40:43 +0900

    fix: fix indent

--- common/util.py
+++ common/util.py
@@ -18,28 +18,28 @@ def preprocess(text):
     return corpus, word_to_id, id_to_word
 
 def create_co_matrix(corpus, vocab_size, window_size=1):
-        corpus_size = len(corpus)
-        co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
-        for idx, word_id in enumerate(corpus):
-                for i in range(1, window_size + 1):
-                        left_idx = idx - i
-                        right_idx = idx + i
+    for idx, word_id in enumerate(corpus):
+            for i in range(1, window_size + 1):
+                    left_idx = idx - i
+                    right_idx = idx + i
 
-                        if left_idx >= 0:
-                                left_word_id = corpus[left_idx]
-                                co_matrix[word_id, left_word_id] += 1
+                    if left_idx >= 0:
+                            left_word_id = corpus[left_idx]
+                            co_matrix[word_id, left_word_id] += 1
 
-                        if right_idx < corpus_size:
-                                right_word_id = corpus[right_idx]
-                                co_matrix[word_id, right_word_id] += 1
+                    if right_idx < corpus_size:
+                            right_word_id = corpus[right_idx]
+                            co_matrix[word_id, right_word_id] += 1
 
-        return co_matrix
+    return co_matrix
 
 def cos_similarity(x, y, eps=1e-8):
-        nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
-        ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
-        return np.dot(nx, ny)
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
 
 def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
     '''類似単語の検索
